a634dc508a9164003f74ea1c63a05df472a3fe35
NoranneGabouge <62263091+NoranneGabouge@users.noreply.github.com>
2020-04-21 02:20:45 -0700

    Test#6 passes (test for a complicated LP with mixed integer variables and several constraints)

--- test/edu/berkeley/aep/LinearProgramTest.java
+++ test/edu/berkeley/aep/LinearProgramTest.java
@@ -44,5 +44,30 @@ public class LinearProgramTest {
         assertFalse(LPb.hasFeasiblePoint(new int[0], new int[]{-1}, new double[]{-1}));
     }
 
+    @Test
+    /*LPc : min of 3x1+2x2-y1-4y2-5y3+8w1-w2,
+    such that
+    x1+x2<=1
+    y1-2y2+y3>=5
+    10y2-w1<=3
+    w2>=9
+    with
+    x1,x2 binary ; y1,y2,y3 integer ; w1,w2 real
+    Test for point=(1,0,6,-1,2,4.5,12.1)
+     */
+    public void pointIsFeasibleForLPc(){
+        LinearProgram LPc=new LinearProgram(2,3,2,Sense.MIN);
+        LPc.setObjective(new double[]{3,2,-1,-4,-5,8,-1});
+        Constraint constr1=new Constraint(new double[]{1,1,0,0,0,0,0},ConstraintSense.LESSTHAN, 1);
+        Constraint constr2=new Constraint(new double[]{0,0,1,-2,1,0,0},ConstraintSense.GREATERTHAN, 5);
+        Constraint constr3=new Constraint(new double[]{0,0,0,10,0,-1,0},ConstraintSense.LESSTHAN, 3);
+        Constraint constr4=new Constraint(new double[]{0,0,0,0,0,0,1},ConstraintSense.GREATERTHAN, 9);
+        LPc.addConstraints(constr1);
+        LPc.addConstraints(constr2);
+        LPc.addConstraints(constr3);
+        LPc.addConstraints(constr4);
+        assertTrue(LPc.hasFeasiblePoint(new int[]{1,0}, new int[]{6,-1,2}, new double[]{4.5,12.1}));
+    }
+
 
 }
